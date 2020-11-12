ee30759293fa4ff53482cfaea3140b58dd18dfb6
RafiP <rafi.panji.p@gmail.com>
2020-11-12 20:31:42 +0700

    Push to new repo

---
+++ client/src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center" style="margin-top: 15%">
+      <div class="card shadow p-2 align-items-center" >
+          <div class="card-body text-center">
+              <h4 class="m-3">PECAH TELOR</h4>
+              <form @submit.prevent="login">
+                  <div class="form-group ">
+                      <label >
+                          Username
+                      </label>
+                      <input
+                          v-model="email"
+                          class="form-control text-purple"
+                          type="text"
+                      >
+                  </div>
+
+                  <button type="submit" class="btn btn-primary btn-block">Play</button>
+              </form>
+          </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Login'
+}
+</script>
---
+++ server/app.js
@@ -0,0 +1,19 @@
+  const express = require('express')
+
+  const cors = require('cors')
+const { isObject } = require('util')
+  const app = express()
+  const PORT = 3000
+  const http = require('http').createServer(app)
+
+  app.use(cors())
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+
+  io.on('connection', (socket) => {
+      console.log('a user connected')
+  })
+
+  http.listen(PORT, () => {
+    console.log(`Telor Pecah app listening at http://localhost:${PORT}`)
+  })
